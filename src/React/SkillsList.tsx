bc48a4c59cb351fe0f888fee5d9b35eacda36750
AhmedSawwah <aelsawwah@yahoo.com>
2026-09-20 04:05:04 -0400

    Update experience heading

--- src/React/SkillsList.tsx
+++ src/React/SkillsList.tsx
@@ -39,7 +39,7 @@ const SkillsList = () => {
   return (
     <div className="text-left pt-3 md:pt-9">
       <h3 className="text-[var(--white)] text-3xl md:text-4xl font-semibold md:mb-6">
-        What I do?
+        My Experience:
       </h3>
       <ul className="space-y-4 mt-4 text-lg">
         {Object.entries(skills).map(([category, items]) => (
